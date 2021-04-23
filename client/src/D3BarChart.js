96418b6023daa3ee37e525c006678aa9b8af61cc
Conor Dixon <conor.dixon@gmail.com>
2021-04-23 09:16:37 +0100

    Database column format and d3 notes

--- client/src/D3BarChart.js
+++ client/src/D3BarChart.js
@@ -1,5 +1,31 @@
 import * as d3 from 'd3';
 
+/*
+Four columns
+http_header - "GET" or "POST"
+
+api_uri -
+https://sandbox.api.mastercard.com/fraud/merchant/v3/termination-inquiry?PageLength=10&Format=JSON
+https://sandbox.api.mastercard.com/atms/v1/atm?PageOffset=0&PageLength=5&AddressLine1=114%20Fifth%20Avenue&AddressLine2=Apartment%201&City=New%20York%20City&CountrySubdivision=NY&PostalCode=11101&Country=USA&Latitude=38.76006576913497&Longitude=-90.74615107952418&DistanceUnit=MILE&Radius=25&SupportEMV=1&InternationalMaestroAccepted=1
+
+status_code_response
+200
+400
+
+time_stamp
+
+"2021-03-27 14:44:23.735000"
+
+formatSecond = d3.timeFormat(":%S"),
+formatMinute = d3.timeFormat("%I:%M"),
+formatHour = d3.timeFormat("%I %p"),
+formatDay = d3.timeFormat("%a %d"),
+formatWeek = d3.timeFormat("%b %d"),
+formatMonth = d3.timeFormat("%B"),
+formatYear = d3.timeFormat("%Y");
+
+*/
+
 
 export default class D3BarChart {
     constructor(element) {
